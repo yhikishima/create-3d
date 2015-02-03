c7a8617a805cb3a41e6f7aee4361fd0c2788cbe0
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-02-04 00:26:26 +0900

    to try unityJavascript

---
+++ Unity/00_gameTest/Assets/sample.js
@@ -0,0 +1,50 @@
+﻿#pragma strict
+
+public var number1: int = 5;
+public var number2: int = 8;
+
+function Start() {
+	 Debug.Log(number1 + number2);
+	 Debug.Log(33);
+}
+
+function Update() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
